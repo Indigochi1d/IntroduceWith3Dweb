b95433ad7b590759e0b8941c99ea9044041c1555
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2024-08-15 02:07:52 +0900

    [feat] add star texture and some meshes

--- src/components/Dancer.jsx
+++ src/components/Dancer.jsx
@@ -1,11 +1,12 @@
 /*eslint-disable react/no-unknown-property*/
-import {useAnimations, useGLTF, useScroll} from "@react-three/drei";
-import {useEffect, useRef} from "react";
+import {Box, Circle, Points, useAnimations, useGLTF, useScroll, useTexture} from "@react-three/drei";
+import {useEffect, useMemo, useRef} from "react";
 import {useRecoilValue} from "recoil";
 import {IsEnteredAtom} from "../stores/index.js";
 import {Loader} from "./Loader.jsx";
 import {useFrame, useThree} from "@react-three/fiber";
 import gsap from "gsap";
+import * as THREE from "three";
 
 let timeLine;
 export const Dancer = () => {
@@ -15,7 +16,18 @@ export const Dancer = () => {
     const {scene,animations} = useGLTF("/models/dancer.glb");
     const {actions} = useAnimations(animations,dancerRef);
     // eslint-disable-next-line no-unused-vars
-    const scroll = useScroll()
+    const scroll = useScroll();
+    const texture = useTexture("/texture/star.png");
+
+    const {positions} = useMemo(()=> {
+        const count = 500;
+        const positions = new Float32Array(count * 3);
+        for(let i=0;i<count*3;i+=1){
+            positions[i]=(Math.random()-0.5) * 25;
+        }
+        return {positions};
+    },[])
+
 
     useFrame(()=>{
         if(!isEntered) return null;
@@ -58,8 +70,67 @@ export const Dancer = () => {
     if(isEntered){
         return(
             <>
-                <ambientLight intensity={2}/>
                 <primitive ref={dancerRef} object={scene} scale={0.05}/>
+                <ambientLight intensity={2}/>
+                <rectAreaLight position={[0,10,0]}
+                               intensity={30}/>
+                <pointLight intensity={45}
+                            position={[0,5,0]}
+                            castShadow={true}
+                            receiveShadow={true}
+                />
+                <hemisphereLight
+                    position={[0,5,0]}
+                    intensity={0}
+                    groundColor={"lime"}
+                    colors="blue"
+                />
+                <Box position={[0,0,0]} args={[100,100,100]}>
+                    <meshStandardMaterial
+                        color={"#DC4F00"}
+                        side={THREE.DoubleSide}/>
+                </Box>
+                <Circle
+                    castShadow={true}
+                    receiveShadow={true}
+                    args={[8,32]}
+                    rotation-x={-Math.PI/2}
+                    position-y={-4.4}
+                >
+                    <meshStandardMaterial
+                    color={"#DC4F00"}
+                    side={THREE.DoubleSide}/>
+                </Circle>
+                <Points positions={positions.slice(0,positions.length/3)}>
+                    <pointsMaterial size={0.5}
+                                   color={new THREE.Color('#DC4F00')}
+                                   sizeAttenuation={true}
+                                   depthWrite={true}
+                                   alphaMap={texture}
+                                   transparent={true}
+                                   alphaTest={0.001}
+                    />
+                </Points>
+                <Points positions={positions.slice((positions.length)/3,(positions.length*2)/3)}>
+                    <pointsMaterial size={0.5}
+                                   color={new THREE.Color('#DC4F00')}
+                                   sizeAttenuation={true}
+                                   depthWrite={true}
+                                   alphaMap={texture}
+                                   transparent={true}
+                                   alphaTest={0.001}
+                    />
+                </Points>
+                <Points positions={positions.slice((positions.length*2)/3,positions.length)}>
+                    <pointsMaterial size={0.5}
+                                   color={new THREE.Color('#DC4F00')}
+                                   sizeAttenuation={true}
+                                   depthWrite={true}
+                                   alphaMap={texture}
+                                   transparent={true}
+                                   alphaTest={0.001}
+                    />
+                </Points>
             </>
         );
     }
